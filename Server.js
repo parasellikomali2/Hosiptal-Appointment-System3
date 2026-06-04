172e9feb5ba145f9883c8bd4b578e03b09b0bd09
parasellikomali2 <parasellikomali2@gmail.com>
2026-06-04 13:51:01 +0530

    Updated project files

--- Server.js
+++ Server.js
@@ -4,6 +4,9 @@ import cors from "cors";
 
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
+import dns from "dns";
+
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 dotenv.config();
 
