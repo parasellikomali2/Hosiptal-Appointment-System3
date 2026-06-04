bff2f93e845e208181bd07a02d6205f3994e9a37
parasellikomali2 <parasellikomali2@gmail.com>
2026-06-04 09:35:55 +0530

    Updated project files

--- Server.js
+++ Server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 
-app.get("/", (req, res) => {
+app.get("/api/auth", (req, res) => {
   res.send("Hospital API Running");
 });
 
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -10,4 +10,6 @@ const router = express.Router();
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 
+
+
 export default router;
